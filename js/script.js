92ce2f1d53cb1d255b92c00c324a461598628ce2
omartins-zs <gabrielmatheus209@gmail.com>
2023-06-22 12:39:22 -0300

    Criando Function Verifica Active no Accordion Item

--- js/script.js
+++ js/script.js
@@ -11,3 +11,21 @@ accordion_item.forEach((item) => {
         VerifyActive(item, accordion_content_item, item_actived);
     });
 });
+
+function VerifyActive(item, content, content_actived) {
+    const icon_item = item.querySelector(".icon");
+    const icon_item_active = document.querySelectorAll(".icon");
+
+    icon_item_active.forEach((item) => (item.innerHTML = "+"));
+
+    if (content_actived) {
+        content_actived.style.height = 0;
+        content_actived.classList.remove("active");
+    }
+
+    if (content !== content_actived) {
+        icon_item.innerHTML = "-";
+        content.classList.add("active");
+        content.style.height = content.scrollHeight + 10 + "px";
+    }
+}
